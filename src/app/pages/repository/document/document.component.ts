446b6fb2020d19472ea481e7454b7cc76174ecc4
kevinmuchcco <kevinmuchcco@gmail.com>
2024-07-09 15:20:28 -0500

    repositorio store

--- src/app/pages/repository/document/document.component.ts
+++ src/app/pages/repository/document/document.component.ts
@@ -14,6 +14,9 @@ export class DocumentComponent {
   page: number = 1;
   pageSize: number = 10;
 
+  activeNodeId: number | null = null;
+  selectedPath: string = '';
+
 
   constructor (private documentService: DocumentService) {}
 
@@ -25,9 +28,9 @@ export class DocumentComponent {
   getTreeview(): void {
     this.documentService.getTreeview().subscribe(
       response => {
-        if (response.status == true) {
+        if (response.status === true) {
           this.documents = response.data;
-          console.log(this.documents);
+          // console.log(this.documents);
         } else {
           console.error('Error:', response.message);
         }
@@ -38,17 +41,52 @@ export class DocumentComponent {
     );
   }
 
-  getDetail(): void {
-    this.documentService.getDetail().subscribe(
+  getDetail(category: string = ''): void {
+    this.documentService.getDetail(category).subscribe(
       response => {
         if (response.status == true) {
-          this.details = response.category
-          // console.log(this.details, "detalles");
-        }else{
+          this.details = response.category;
+          // console.log(this.details);
+        } else {
           console.error('Error:', response.message);
         }
+      },
+      error => {
+        console.error('Error:', error);
       }
     );
   }
 
+  searchByCategory(categoryId: number, node: any): void {
+    this.getDetail(categoryId.toString());
+    this.activeNodeId = categoryId; // Set the active node ID
+    this.selectedPath = this.getNodePath(categoryId, this.documents, ''); // Update the selected path
+  }
+
+  isActive(nodeId: number): boolean {
+    return this.activeNodeId === nodeId;
+  }
+
+  getStyle(nodeId: number): any {
+    return {
+      'text-decoration': this.isActive(nodeId) ? 'underline' : 'none'
+    };
+  }
+
+  getNodePath(nodeId: number, nodes: any[], path: string): string {
+    for (const node of nodes) {
+      console.log(nodeId)
+      if (node.id === nodeId) {
+        return `${path} > ${node.id} - ${node.denominacion}`;
+      }
+      if (node.children?.length) {
+        const result = this.getNodePath(nodeId, node.children, `${path} > ${node.id} - ${node.denominacion}`);
+        console.log(result);
+        if (result) {
+          return result;
+        }
+      }
+    }
+    return path;
+  }
 }
--- src/app/pages/repository/document/service/document.service.ts
+++ src/app/pages/repository/document/service/document.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams  } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 
@@ -16,7 +16,11 @@ export class DocumentService {
     return this.http.get<any>(`${this.apiUrl}/repository-treeview`);    
   }
 
-  getDetail(): Observable<any> {    
-    return this.http.get<any>(`${this.apiUrl}/repository-list`);
+  getDetail(category: string = ''): Observable<any> {
+    let params = new HttpParams();
+    if (category) {
+      params = params.set('category', category);
+    }
+    return this.http.get<any>(`${this.apiUrl}/repository-list`, { params });
   }
 }
